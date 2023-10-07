d49fcc504a45b28a62067ddbeb5b7e563cacc139
Terminal.cs <76945439+terminal-cs@users.noreply.github.com>
2023-10-07 13:59:52 -0700

    Update Configuration.h

--- Firmware/Configuration/Configuration.h
+++ Firmware/Configuration/Configuration.h
@@ -3103,6 +3103,8 @@
 //
 //#define BTT_TFT35_SPI_V1_0
 
+#define CR10_STOCKDISPLAY
+
 //
 // Generic TFT with detailed options
 //
@@ -3132,7 +3134,7 @@
  *   For LVGL_UI also copy the 'assets' folder from the build directory to the
  *   root of your SD card, together with the compiled firmware.
  */
-//#define TFT_CLASSIC_UI
+#define TFT_CLASSIC_UI
 //#define TFT_COLOR_UI
 //#define TFT_LVGL_UI
 
