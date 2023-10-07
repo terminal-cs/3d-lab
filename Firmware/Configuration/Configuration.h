f173eb9c1f4f006a2c04e71183b842ee3695815a
Terminal.cs <76945439+terminal-cs@users.noreply.github.com>
2023-10-07 15:40:17 -0700

    Update Configuration.h

--- Firmware/Configuration/Configuration.h
+++ Firmware/Configuration/Configuration.h
@@ -1702,7 +1702,7 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS -55
+#define X_MIN_POS -50
 #define Y_MIN_POS -15
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
