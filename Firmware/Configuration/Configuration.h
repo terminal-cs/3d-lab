f73988d8b0e1be0c2f5d9b712e268621071b850a
Terminal.cs <76945439+terminal-cs@users.noreply.github.com>
2023-10-07 16:31:35 -0700

    Update Configuration.h

--- Firmware/Configuration/Configuration.h
+++ Firmware/Configuration/Configuration.h
@@ -1705,7 +1705,7 @@
 #define X_MIN_POS -50
 #define Y_MIN_POS -10
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
+#define X_MAX_POS 180
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 220
 //#define I_MIN_POS 0
